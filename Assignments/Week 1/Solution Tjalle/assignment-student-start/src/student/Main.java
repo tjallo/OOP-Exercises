99a54e4d55cea5f40b08fc564b849e773380a6c0
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-01-27 08:46:09 +0100

    init

---
+++ Assignments/Week 1/Solution Tjalle/assignment-student-start/src/student/Main.java
@@ -0,0 +1,8 @@
+package student;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	System.out.println("Good luck, have fun!");
+    }
+}
---
+++ Assignments/Week 1/Solution Tjalle/assignment-student-start/test/student/Assignment01Tester.java
@@ -0,0 +1,18 @@
+package student;
+
+public class Assignment01Tester {
+
+	public void createGroup(int i) {
+	}
+
+	public void addStudent(int sNumber, String firstName, String lastName) {
+	}
+
+	public void changeStudent(int sNumber, String firstName, String lastName) {
+	}
+
+	public String printStudents() {
+		return "";
+	}
+
+}
