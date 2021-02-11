f39c376fbee67788098680ab49c8940f99f23c4c
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-02-11 13:12:43 +0100

    fixed stupid bug, but all code runs

--- Assignments/Week 3/Solution Tjalle/assignment-geometric-shapes-start/src/geometric/GeometricYComparator.java
+++ Assignments/Week 3/Solution Tjalle/assignment-geometric-shapes-start/src/geometric/GeometricYComparator.java
@@ -9,8 +9,8 @@ public class GeometricYComparator implements Comparator<Geometric> {
 
     @Override
     public int compare(Geometric o1, Geometric o2) {
-        double o1Y = o1.getYval();
-        double o2Y = o2.getYval();
+        double o1Y = o1.bottomBorder();
+        double o2Y = o2.bottomBorder();
 
         if (o1Y < o2Y) {
             return -1;
