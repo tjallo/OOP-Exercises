a3d187da861ca34dd6c4243ef33961a6b4f96983
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-02-11 13:16:55 +0100

    bugfix

--- Assignments/Week 3/Solution Tjalle/assignment-geometric-shapes-start/src/geometric/GeometricXComparator.java
+++ Assignments/Week 3/Solution Tjalle/assignment-geometric-shapes-start/src/geometric/GeometricXComparator.java
@@ -9,8 +9,8 @@ public class GeometricXComparator implements Comparator<Geometric> {
 
     @Override
     public int compare(Geometric o1, Geometric o2) {
-        double o1X = o1.bottomBorder();
-        double o2X = o2.bottomBorder();
+        double o1X = o1.leftBorder();
+        double o2X = o2.leftBorder();
 
         if (o1X < o2X) {
             return -1;
