ef857c7f32fe97690f221fd7bff4ccdc43bdbe13
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-02-27 21:27:55 +0100

    init

---
+++ Assignments/Week 5/Answer Tjalle/assignment-expressions-start/src/expressions/ExpressionFactory.java
@@ -0,0 +1,24 @@
+package expressions;
+
+public class ExpressionFactory {
+
+	public static Expression var(String x) {
+		return null;
+	}
+
+	public static Expression con(Double x) {
+		return null;
+	}
+
+	public static Expression add(Expression x, Expression y) {
+		return null;
+	}
+
+	public static Expression mul(Expression x, Expression y) {
+		return null;
+	}
+
+	public static Expression neg(Expression x) {
+		return null;
+	}
+}
