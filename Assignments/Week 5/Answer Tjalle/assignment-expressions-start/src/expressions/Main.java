35f4fb3bb9a0c207b923520a3e1fbd6544c600ab
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-02-28 15:41:09 +0100

    added some personeal testing stuff

--- Assignments/Week 5/Answer Tjalle/assignment-expressions-start/src/expressions/Main.java
+++ Assignments/Week 5/Answer Tjalle/assignment-expressions-start/src/expressions/Main.java
@@ -1,7 +1,25 @@
 package expressions;
 
+import static expressions.ExpressionFactory.*;
+
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hoi");
+
+		ArrayList<Expression> e = new ArrayList<Expression>();
+
+		e.add(add(mul(con(2.), con(3.)), var("x")));
+		e.add(add(mul(con(2.), var("x")), mul(var("y"), mul(con(2.), con(6.)))));
+
+		printerE(e);
+
+	}
+
+	public static void printerE(ArrayList<Expression> e) {
+		for (Expression ex : e) {
+			System.out.println(ex.toString());
+			System.out.println(ex.partialEval().toString() + "\n");
+		}
 	}
 }
