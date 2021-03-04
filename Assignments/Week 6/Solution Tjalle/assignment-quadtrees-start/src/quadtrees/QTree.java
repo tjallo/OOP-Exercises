3b7ece9e8ada9372894cb936038614dae39d9572
Tjalle <31376864+tjallo@users.noreply.github.com>
2021-03-04 13:45:40 +0100

    init week 6 proper

---
+++ Assignments/Week 6/Solution Tjalle/assignment-quadtrees-start/src/quadtrees/QTree.java
@@ -0,0 +1,33 @@
+package quadtrees;
+
+import java.io.Reader;
+import java.io.Writer;
+
+public class QTree {
+	private QuadTreeNode root;
+
+	public QTree(Reader input) {
+		root = readQTree(input);
+	}
+
+	public QTree(Bitmap bitmap) {
+		root = bitmap2QTree(0, 0, bitmap.getWidth(), bitmap);
+	}
+
+	public void fillBitmap(Bitmap bitmap) {
+		root.fillBitmap(0, 0, bitmap.getWidth(), bitmap);
+	}
+
+	public void writeQTree(Writer sb) {
+		root.writeNode(sb);
+	}
+
+	private static QuadTreeNode readQTree(Reader input) {
+		return null;
+	}
+
+	public static QuadTreeNode bitmap2QTree(int x, int y, int width, Bitmap bitmap) {
+		return null;
+	}
+
+}
