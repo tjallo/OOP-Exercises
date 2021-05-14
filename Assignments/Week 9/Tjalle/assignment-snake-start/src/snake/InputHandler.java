1dd7c042d1b2812c66de845c85f25c11e21c4f65
Tjalle Wolterink <tjalle.wolterink@student.ru.nl>
2021-05-14 12:50:20 +0200

    Added week 12 exercises

--- Assignments/Week 9/Tjalle/assignment-snake-start/src/snake/InputHandler.java
+++ Assignments/Week 9/Tjalle/assignment-snake-start/src/snake/InputHandler.java
@@ -1,27 +1,52 @@
 package snake;
 
 import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 /**
- * Handles controls of a snake game, where the 'a' and 'd' keys can be used to move and 's' (un)pauses the game
+ * Handles controls of a snake game, where the 'a' and 'd' keys can be used to
+ * move and 's' (un)pauses the game
  */
 public class InputHandler {
 
     private final EventHandler<KeyEvent> keyHandler;
     private final EventHandler<MouseEvent> mouseHandler;
 
-    public InputHandler(World world) {
-        Snake snake = world.getSnake();
-
+    public InputHandler(World w) {
+        Snake s = w.getSnake();
         keyHandler = keyEvent -> {
-            // TODO: Implement controls
+
+            if (keyEvent.getCode() == KeyCode.A) {
+
+                s.setDirection(s.getDirection().rotateLeft());
+
+            } else if (keyEvent.getCode() == KeyCode.D) {
+
+                s.setDirection(s.getDirection().rotateRight());
+
+            } else if (keyEvent.getCode() == KeyCode.S) {
+
+                if (w.isRunning()) {
+                    w.setRunning(false);
+                } else {
+                    w.setRunning(true);
+                }
+
+            }
+
             keyEvent.consume();
         };
 
         mouseHandler = mouseEvent -> {
-            // TODO: Implement mouse
+            int x = (int) (mouseEvent.getX() / SnakeGame.SCALE);
+            int y = (int) (mouseEvent.getY() / SnakeGame.SCALE);
+
+            if (x < w.getSize() && y < w.getSize() && x >= 0 && y >= 0) {
+                w.getFood().moveTo(x, y);
+            }
             mouseEvent.consume();
         };
     }
--- Assignments/Week 9/Tjalle/assignment-snake-start/src/snake/SnakeGame.java
+++ Assignments/Week 9/Tjalle/assignment-snake-start/src/snake/SnakeGame.java
@@ -1,8 +1,14 @@
 package snake;
 
+import javafx.beans.binding.Bindings;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
 
 /**
  * A JavaFX Pane that displays the snake game represented by the given world
@@ -11,22 +17,47 @@ public class SnakeGame extends Pane {
 
     public static final int SCALE = 16;
 
-    public SnakeGame(World world) {
-        setPrefSize(world.getSize() * SCALE, world.getSize() * SCALE);
+    public SnakeGame(World w) {
+        setPrefSize(w.getSize() * SCALE, w.getSize() * SCALE);
+        w.getSnake().addListener(segment -> {
+            Rectangle body = new Rectangle(SCALE, SCALE, Color.GREEN);
+            body.xProperty().bind(segment.getXProperty().multiply(SCALE));
+            body.yProperty().bind(segment.getYProperty().multiply(SCALE));
+            getChildren().add(body);
+        });
 
-        // TODO: Implement graphics
+        Rectangle rect1 = new Rectangle(SCALE, SCALE);
+        rect1.setFill(Color.RED);
+        rect1.xProperty().bind(w.getSnake().getXProperty().multiply(SCALE));
+        rect1.yProperty().bind(w.getSnake().getYProperty().multiply(SCALE));
+
+        getChildren().add(rect1);
+
+        Circle circle1 = new Circle(SCALE / 2);
+        circle1.setFill(Color.BLUE);
+        circle1.centerXProperty().bind(w.getFood().getXProperty().multiply(SCALE).add(SCALE / 2));
+        circle1.centerYProperty().bind(w.getFood().getYProperty().multiply(SCALE).add(SCALE / 2));
+
+        getChildren().add(circle1);
     }
 
-    public static Pane createUserInterface(World world) {
-        VBox ui = new VBox();
+    public static Pane createUserInterface(World w) {
+        VBox uivBox = new VBox();
 
-        Label scoreText = new Label();
-        Label runningText = new Label("Press 's' to start");
+        Label scoreLbl = new Label("points");
+        Label runningLbl = new Label("Press 's' to start");
 
-        // TODO: Implement user interface
+        scoreLbl.textProperty().bind(w.getScoreProperty().asString("Your score is %d"));
+        w.getRunningProperty().addListener((e, x, y) ->
+        {
+            if (y) {
+                runningLbl.textProperty().set("");
+            } else runningLbl.textProperty().set("Press s to unpause");
+        });
 
-        ui.getChildren().addAll(scoreText, runningText);
+        uivBox.getChildren().addAll(scoreLbl, runningLbl);
 
-        return ui;
+        return uivBox;
     }
 }
+
