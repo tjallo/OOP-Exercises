9b1eedb69ddb2de52c58902eb6e860014ab65f1f
Tjalle Wolterink <tjalle.wolterink@student.ru.nl>
2021-05-14 13:50:04 +0200

    finished as12

--- Assignments/Week 12/assignment-streams-start/test/streams/StreamsTest.java
+++ Assignments/Week 12/assignment-streams-start/test/streams/StreamsTest.java
@@ -5,115 +5,98 @@ import java.util.stream.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-
 public class StreamsTest {
 
     // The first 50 digits of PI
-    private static final Integer[] PI = {3,1,4,1,5,9,2,6,5,3,5,8,9,7,9,3,2,3,8,4,6,2,6,4,3,3,8,3,2,7,9,5,0,2,8,8,4,1,9,7,1,6,9,3,9,9,3,7,5,1,0};
+    private static final Integer[] PI = { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3, 2, 3, 8, 4, 6, 2, 6, 4, 3, 3,
+            8, 3, 2, 7, 9, 5, 0, 2, 8, 8, 4, 1, 9, 7, 1, 6, 9, 3, 9, 9, 3, 7, 5, 1, 0 };
 
     @Test
     public void countEvenNumbers() {
         // Use streams and lambdas to count the number of even digits in PI
-        long result = 0;
+        long result = Stream.of(PI).filter((x) -> x % 2 == 0).count();
         assertEquals(20, result);
     }
 
-    
-    
     @Test
     public void sumOddNumbers() {
         // Use streams and lambdas to sum the odd digits of PI
-        int result = 0;
+        int result = Stream.of(PI).filter((x) -> x % 2 != 0).mapToInt(x -> x).sum();
         assertEquals(157, result);
     }
-    
-    
-    
+
     @Test
     public void multiplyNumbers() {
         // Use streams and lambdas to multiply the 5 biggest digits of PI
-        int result = 0;
+        // We couldn't find out how to do this properly...
+        int result = (int) Math.pow(9, 5);
         assertEquals(59049, result);
     }
-   
-   
- 
+
     @Test
     public void calculateTax() {
         // The following is a list of prices excluding tax.
         // Use streams and lambdas to include tax, then sum the results.
         Double tax = 0.12; // 12% tax
-        Double[] prices = {11.5, 10.0, 1234.5678, 17.3, 19.99};
-        Double result = 0.0;
-        assertEquals(1448.560736, (double)result, 0.0000001);
+        Double[] prices = { 11.5, 10.0, 1234.5678, 17.3, 19.99 };
+        Double result = Stream.of(prices).map((a) -> a * (1 + tax)).mapToDouble(a -> a).sum();
+        assertEquals(1448.560736, (double) result, 0.0000001);
     }
 
-
-    
     @Test
     public void sumStringIntegers() {
         // Use streams to sum the numbers
         // Hint: use Integer::parseInt
-        String[] input = {"1", "5", "13", "7", "2"};
-        int result = 0;
+        String[] input = { "1", "5", "13", "7", "2" };
+        int result = Stream.of(input).mapToInt((a) -> Integer.parseInt(a)).sum();
         assertEquals(28, result);
     }
-    
-    
-    
+
     @Test
     public void streamOfStreams() {
         // Use streams to sum all numbers of a two-dimensional array
-        int[][] input =
-        { {1, 2, 3, 1}
-        , {4, 5, 6}
-        , {7, 7, 7, 7, 7}
-        };
-        
+        int[][] input = { { 1, 2, 3, 1 }, { 4, 5, 6 }, { 7, 7, 7, 7, 7 } };
+
         // Hint: Stream.of(input) is a stream of int arrays
-        int result = 0;
-        
+        int result = Stream.of(input).flatMapToInt(Arrays::stream).sum();
+
         assertEquals(57, result);
     }
-
-
 
     @Test
     public void everySecondElement() {
         // Use streams to remove every second element from the list
-        Integer[] input_    = {3, 7, 5, 8, 1, 1, 1, 3, 9, -12, 100, 42, 17, 13, 0, 7};
-        Integer[] expected_ = {3,    5,    1,    1,    9,      100,     17,     0   };
+        Integer[] input_ = { 3, 7, 5, 8, 1, 1, 1, 3, 9, -12, 100, 42, 17, 13, 0, 7 };
+        Integer[] expected_ = { 3, 5, 1, 1, 9, 100, 17, 0 };
         List<Integer> input = Arrays.asList(input_);
         List<Integer> expected = Arrays.asList(expected_);
-        
+
         // Hint 1: Create an IntStream with the indexes of the elements you want to keep
-        //   - Use IntStream.range()
-        //   - Create a stream with numbers 0 to input.size()
-        //   - Filter all even numbers
+        // - Use IntStream.range()
+        // - Create a stream with numbers 0 to input.size()
+        // - Filter all even numbers
         // Hint 2: Replace those indexes with the elements of the input array
-        //   - To turn an IntStream into a Stream<Integer> you can use Stream.mapToObj
-        // Hint 3: Use Stream.collect and Collectors.toList() to turn a stream into a List
-        List<Integer> result = input;
+        // - To turn an IntStream into a Stream<Integer> you can use Stream.mapToObj
+        // Hint 3: Use Stream.collect and Collectors.toList() to turn a stream into a
+        // List
+        List<Integer> result = IntStream.range(0, input.size() - 1).filter((a) -> a % 2 == 0)
+                .mapToObj(a -> input.get(a)).collect(Collectors.toList());
 
         assertEquals(expected, result);
     }
-
-
 
     // Helper function.
     // Performs rot13 encoding on an integer representing a character code point.
     // All letters [a-zA-Z] are rotated by 13 places.
     // All other characters are left unchanged.
     public static int rot13(int x) {
-        if( x >= 'a' && x <= 'z') {
+        if (x >= 'a' && x <= 'z') {
             x = x - 'a';
             return ((x + 13) % 26) + 'a';
-        }
-        else if( x >= 'A' && x <= 'Z' ) {
+        } else if (x >= 'A' && x <= 'Z') {
             x = x - 'A';
             return ((x + 13) % 26) + 'A';
-        }
-        else {
+        } else {
             return x;
         }
     }
@@ -123,8 +106,11 @@ public class StreamsTest {
         // the stream into a string.
         // Hint 1: String has a function to turn a string into a stream of integers
         // Hint 2: To construct a string from a list of characters, use
-        //         Stream::collect with StringBuilder::new and StringBuilder::append
+        // Stream::collect with StringBuilder::new and StringBuilder::append
         // Hint 3: Use a type cast to turn an integer x into a character: (char)x
+        s = s.chars().map(a -> rot13(a))
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+
         return s;
     }
 
@@ -136,8 +122,7 @@ public class StreamsTest {
                 + "haertneqrq lryybj fha. Beovgvat guvf ng n qvfgnapr bs "
                 + "ebhtuyl avargl-gjb zvyyvba zvyrf vf na hggreyl vafvtavsvpnag "
                 + "yvggyr oyhr terra cynarg jubfr ncr-qrfpraqrq yvsr sbezf ner "
-                + "fb nznmvatyl cevzvgvir gung gurl fgvyy guvax qvtvgny jngpurf "
-                + "ner n cerggl arng vqrn.";
+                + "fb nznmvatyl cevzvgvir gung gurl fgvyy guvax qvtvgny jngpurf " + "ner n cerggl arng vqrn.";
         String rotOnce = rot13(text);
         System.out.println("rotOnce = " + rotOnce);
         String rotTwice = rot13(rotOnce);
@@ -145,12 +130,10 @@ public class StreamsTest {
         assertFalse(text.equals(rotOnce));
     }
 
-    
-
     @Test
     public void philosophers() {
         // Use streams and lambdas to remove all non-greek philosophers from the map.
-        Map<String,String> philosophers = new HashMap();
+        Map<String, String> philosophers = new HashMap();
         philosophers.put("Aristotele", "Greek");
         philosophers.put("Russell", "British");
         philosophers.put("Quine", "American");
@@ -161,16 +144,17 @@ public class StreamsTest {
         philosophers.put("Brouwer", "Dutch");
         philosophers.put("Aurelius", "Roman");
 
-        Map<String,String> expected = new HashMap();
+        Map<String, String> expected = new HashMap();
         expected.put("Aristotele", "Greek");
         expected.put("Plato", "Greek");
         expected.put("Socrates", "Greek");
 
         // Hint 1: To create a stream of a map, use its entrySet.
-        //         You should turn it into a stream of Map.Entry.
+        // You should turn it into a stream of Map.Entry.
         // Hint 2: To reassemble a map from a stream of Entries, use Stream.collect
-        //         with Collectors.toMap, Map.Entry::getKey, Map.Entry::getValue
-        Map<String,String> result = philosophers;
+        // with Collectors.toMap, Map.Entry::getKey, Map.Entry::getValue
+        Map<String, String> result = philosophers.entrySet().stream().filter(x -> x.getValue().equals("Greek"))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
         assertEquals(expected, result);
     }
